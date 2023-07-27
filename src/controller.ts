85eb0923e2ad610a9b9ff1d7737e824810146639
Bouchekirredouane <redouane.bouchekir@gmail.com>
2023-07-27 16:51:01 +0200

    new version 27 july

--- src/controller.ts
+++ src/controller.ts
@@ -2,27 +2,33 @@ import * as vscode from 'vscode';
 import * as cp from 'child_process';
 
 
+import AbortController from "abort-controller";
+
+
 export class SampleKernel {
-	private readonly _id = 'test-notebook-serializer-kernel';
+	private readonly _id = 'etb2-notebook-serializer-kernel';
 	private readonly _label = 'ETB2 Kernel';
-	private readonly _supportedLanguages = ['json'];
+	private readonly _supportedLanguages = [];
 
 	private _executionOrder = 0;
 	private readonly _controller: vscode.NotebookController;
+
+	private abortController: AbortController | undefined;
 
 	constructor() {
 		this._controller = vscode.notebooks.createNotebookController(this._id,
 			'etb2-notebook',
 			this._label);
-		
 		this._controller.supportedLanguages = this._supportedLanguages;
 		this._controller.supportsExecutionOrder = true;
 		this._controller.executeHandler = this._executeAll.bind(this);
+		
 	}
 
 	dispose(): void {
 		this._controller.dispose();
 	}
+
 
 	private _executeAll(cells: vscode.NotebookCell[], _notebook: vscode.NotebookDocument, _controller: vscode.NotebookController): void {
 		for (const cell of cells) {
@@ -37,8 +43,11 @@ export class SampleKernel {
 			execution.executionOrder = ++this._executionOrder;
 			execution.start(Date.now());
 			try {
+
+				const errorText = "Error: Please save the etb2 notebook first to specify the working directory.";
+				const formattedErrorText = `<span style="color:#ff0000">Error: </span> ${errorText.substring("Error:".length)}`;
 				execution.replaceOutput([new vscode.NotebookCellOutput([
-					vscode.NotebookCellOutputItem.text("Error: Please save the etb2 notebook first to specify the working directory. ")
+					vscode.NotebookCellOutputItem.text(formattedErrorText, 'text/html')
 				])]);
 				execution.end(true, Date.now());
 			} catch (err) {
@@ -49,45 +58,88 @@ export class SampleKernel {
 			}
 		}
 		
-		else{
-			execution.executionOrder = ++this._executionOrder;
-			execution.start(Date.now());
-			try {
-				execution.replaceOutput([new vscode.NotebookCellOutput([
-					vscode.NotebookCellOutputItem.text(executeETB2Command(cell.document.getText(), cell.notebook.uri.toString()))
-				])]);
-				execution.end(true, Date.now());
-			} catch (err) {
-				execution.replaceOutput([new vscode.NotebookCellOutput([
-					vscode.NotebookCellOutputItem.error(err as Error)
-				])]);
-				execution.end(false, Date.now());
-			}
-		}
-
+		else {
+            execution.executionOrder = ++this._executionOrder;
+            execution.start(Date.now());
+            try {
+                this.abortController = new AbortController();
+                execution.token.onCancellationRequested(_ => this.abortExecution());
+                
+                execution.replaceOutput([new vscode.NotebookCellOutput([
+                    vscode.NotebookCellOutputItem.text(await executeETB2Command(cell.document.getText(), cell.notebook.uri.toString(), this.abortController))
+                ])]);
+                execution.end(true, Date.now());
+            } catch (err) {
+                execution.replaceOutput([new vscode.NotebookCellOutput([
+                    vscode.NotebookCellOutputItem.error(err as Error)
+                ])]);
+                execution.end(false, Date.now());
+            }
+        }
 	}
+
+	private abortExecution() {
+        if (this.abortController) {
+            this.abortController.abort();
+            vscode.window.showInformationMessage("Execution aborted.");
+        }
+    }
+
 }
 
 
-function executeETB2Command(etb2Command: string, workingDirectory:string): string{
-	
-	// Example: file:///home/bouchekir/Desktop/vc-etb-user-test/certibus.sample-etb2-notebook
-	const pathWithoutFilePrefix = workingDirectory.replace("file:///", "");
-	const pathWithoutFinalPart = pathWithoutFilePrefix.replace(/\/[^/]*$/, "");
-	vscode.window.showInformationMessage("I get:  "+pathWithoutFinalPart);
-	const command = `/bin/bash -i -c "cd /${pathWithoutFinalPart} && ${etb2Command} 2>&1"`;
 
-	  
-	try {
-	  //const result = cp.execSync('bash -i -c "'+etb2Command+'"', { stdio: 'pipe' }).toString().trim();
-	  //const result = cp.execSync(etb2Command, { stdio: 'pipe', shell: '/bin/bash' }).toString().trim(); 
-	  //const result = cp.execSync('/bin/bash -i -c "'+etb2Command+'"', { stdio: 'pipe' }).toString().trim();
-	  //const result = cp.execSync('/bin/bash -i -c "' + etb2Command + ' 2>&1"', { stdio: 'pipe' }).toString().trim();
-	  const result = cp.execSync(command, { stdio: 'pipe' }).toString().trim();
-	  return result.trim();;
-	} catch (error: any) {
-		const errorMessage = (error.message as string)?.trim() || "Unknown Error";
-		vscode.window.showInformationMessage('Error occurred:'+ error.message);
-    	return "Error" + errorMessage;
-	}
+
+
+async function executeETB2Command(
+	etb2Command: string,
+	workingDirectory: string,
+	abortController: AbortController
+  ): Promise<string> {
+	const pathWithoutFilePrefix = workingDirectory.replace('file:///', '');
+	const pathWithoutFinalPart = pathWithoutFilePrefix.replace(/\/[^/]*$/, '');
+	const escapedSpecialCar = etb2Command.replace(/"/g, '\\"'); // Escape double quotes with backslashes
+	const command = `/bin/bash -i -l -c "cd /${pathWithoutFinalPart} && ${escapedSpecialCar} 2>&1"`;
+	vscode.window.showInformationMessage('Running ETB2 command:  ' + etb2Command);
+  
+	return new Promise<string>((resolve, reject) => {
+	  const childProcess = cp.spawn(command, [], {
+		shell: true,
+		stdio: 'pipe',
+	  });
+  
+	  let result = '';
+  
+	  // Listen for the 'data' event to capture the output of the child process
+	  childProcess.stdout?.on('data', (data) => {
+		result += data.toString();
+	  });
+  
+	  // Listen for the 'error' event to handle any errors that occur during child process execution
+	  childProcess.on('error', (error) => {
+		vscode.window.showInformationMessage('Error occurred:' + error.message);
+		reject(error);
+	  });
+  
+	  // Listen for the 'abort' event from the AbortController to manually kill the child process
+	  abortController.signal.addEventListener('abort', () => {
+		if (childProcess.pid) {
+		  process.kill(childProcess.pid, 'SIGABRT');
+		  vscode.window.showInformationMessage("kill PID: "+childProcess.pid);
+		}
+	  });
+  
+	  // Listen for the 'exit' event to resolve or reject the promise when the child process is done
+	  childProcess.on('exit', (code, signal) => {
+		if (signal === 'SIGABRT') {
+		  reject(new Error('Command execution was aborted.'));
+		} else if (code === 0) {
+		  resolve(result.trim());
+		} else {
+		  reject(new Error(`Command execution failed with exit code ${code}.`));
+		}
+	  });
+	});
   }
+  
+
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@ import * as vscode from 'vscode';
 import { SampleKernel } from './controller';
 import { SampleContentSerializer } from './serializer';
 
+import { ExtensionContext, StatusBarAlignment, window, StatusBarItem, Selection, workspace, TextEditor, commands } from 'vscode';
+import { basename } from 'path';
+
 import * as cp from 'child_process';
 
 
@@ -19,38 +22,58 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 
-	let contr = new SampleKernel(); 
+	let etb2controller = new SampleKernel(); 
 	context.subscriptions.push(
 		vscode.workspace.registerNotebookSerializer(
 			NOTEBOOK_TYPE, new SampleContentSerializer(), { transientOutputs: true }
 		),
-		contr
+		etb2controller
 	);
 
+
 	context.subscriptions.push(vscode.commands.registerCommand('etb2.createNotebook', async () => {
-		const language = 'json';
-			const defaultValue = `etb2 -h`;
-			const cell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, defaultValue, language);
-			const data = new vscode.NotebookData([cell]);
-			data.metadata = {
-				custom: {
-					cells: [],
-					metadata: {
-						orig_nbformat: 4
-					},
-					nbformat: 4,
-					nbformat_minor: 2
-				}
-			};
-			const doc = await vscode.workspace.openNotebookDocument(NOTEBOOK_TYPE, data);
-			await vscode.window.showNotebookDocument(doc);
+		const defaultValue = `etb2 -h`;
+		const cell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, defaultValue, 'shellscript');
+		const data = new vscode.NotebookData([cell]);
+		data.metadata = {
+			custom: {
+				cells: [],
+				metadata: {
+					orig_nbformat: 4
+				},
+				nbformat: 4,
+				nbformat_minor: 2
+			}
+		};
+		const doc = await vscode.workspace.openNotebookDocument(NOTEBOOK_TYPE, data);
+		await vscode.window.showNotebookDocument(doc);
 	}));
 
 
+
+	// Create a status bar item
+	const status = window.createStatusBarItem(StatusBarAlignment.Left, 1000000);
+	context.subscriptions.push(status);
+
+	// Update status bar item based on events for multi root folder changes
+	context.subscriptions.push(workspace.onDidChangeWorkspaceFolders(e => updateStatus(status)));
+
+	// Update status bar item based on events for configuration
+	context.subscriptions.push(workspace.onDidChangeConfiguration(e => updateStatus(status)));
+
+	// Update status bar item based on events around the active editor
+	context.subscriptions.push(window.onDidChangeActiveTextEditor(e => updateStatus(status)));
+	context.subscriptions.push(window.onDidChangeTextEditorViewColumn(e => updateStatus(status)));
+	context.subscriptions.push(workspace.onDidOpenTextDocument(e => updateStatus(status)));
+	context.subscriptions.push(workspace.onDidCloseTextDocument(e => updateStatus(status)));
+
+	updateStatus(status);
+
+
+
+
+
 }
-
-
-
 
 function isETB2Installed(): boolean {
 	
@@ -65,4 +88,49 @@ function isETB2Installed(): boolean {
 
 
 
+
+  function updateStatus(status: StatusBarItem): void {
+	const info = getEditorInfo();
+	status.text = info ? info.text || '' : '';
+	status.tooltip = info ? info.tooltip : undefined;
+	status.color = info ? info.color : undefined;
+
+	if (info) {
+		status.show();
+	} else {
+		status.hide();
+	}
+}
+
+function getEditorInfo(): { text?: string; tooltip?: string; color?: string; } | null {
+	const editor = window.activeTextEditor;
+
+	// If no workspace is opened or just a single folder, we return without any status label
+	// because our extension only works when more than one folder is opened in a workspace.
+	if (!editor || !workspace.workspaceFolders || workspace.workspaceFolders.length < 2) {
+		return null;
+	}
+
+	let text: string | undefined;
+	let tooltip: string | undefined;
+	let color: string | undefined;
+
+	// If we have a file:// resource we resolve the WorkspaceFolder this file is from and update
+	// the status accordingly.
+	const resource = editor.document.uri;
+	if (resource.scheme === 'file') {
+		const folder = workspace.getWorkspaceFolder(resource);
+		if (!folder) {
+			text = `$(alert) <outside workspace> → ${basename(resource.fsPath)}`;
+		} else {
+			text = `$(file-submodule) ${basename(folder.uri.fsPath)} (${folder.index + 1} of ${workspace.workspaceFolders.length}) → $(file-code) ${basename(resource.fsPath)}`;
+			tooltip = resource.fsPath;
+
+			const multiRootConfigForResource = workspace.getConfiguration('multiRootSample', resource);
+			color = multiRootConfigForResource.get('statusColor');
+		}
+	}
+
+	return { text, tooltip, color };
+}
 
